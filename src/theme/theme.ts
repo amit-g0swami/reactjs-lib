4cb04cc5fbf1b573804b7e168a26ea0e8f673415
am1t-kumar <thorragnarak@gmail.com>
2024-08-18 10:37:33 +0530

    add: theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,28 @@
+export const theme = {
+  baseTheme: {
+    colors: {
+      blue: '#1fb6ff',
+      purple: '#7e5bef',
+      pink: '#ff49db',
+      orange: '#ff7849',
+      green: '#13ce66',
+      yellow: '#ffc82c',
+      'gray-dark': '#273444',
+      gray: '#8492a6',
+      'gray-light': '#d3dce6'
+    }
+  },
+  darkTheme: {
+    colors: {
+      blue: '#1fb6ff',
+      purple: '#7e5bef',
+      pink: '#ff49db',
+      orange: '#ff7849',
+      green: '#13ce66',
+      yellow: '#ffc82c',
+      'gray-dark': '#d3dce6',
+      gray: '#8492a6',
+      'gray-light': '#273444'
+    }
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
 const plugin = require('tailwindcss/plugin')
+import { theme } from './src/theme'
 
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
+      ...theme.baseTheme,
       dark: {
+        ...theme.darkTheme
       }
     }
   },
