196d013444addeb89e23eaccea525a40647708da
am1t-kumar <thorragnarak@gmail.com>
2024-08-18 13:08:08 +0530

    add: react app entry point

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { TextInput } from './components'
+
+function App() {
+  return <TextInput disabled value="Hello World!" />
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
+
+// // react app entry point
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
+// export all components, hooks, theme, types, and utility
+export * from './components'
+export * from './hooks'
+export * from './theme'
+export * from './types'
+export * from './utility'
